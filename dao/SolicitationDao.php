6fded786ab6883a9ee3f1e8256b4d1990965351d
francismoura <francis.gulla@gmail.com>
2019-07-22 14:56:32 -0300

    Modal funcionando corretamente

--- dao/SolicitationDao.php
+++ dao/SolicitationDao.php
@@ -6,65 +6,65 @@ require_once 'DAO.php';
 
 abstract class SolicitationDao implements DAO
 {
-    protected $tableDB = 'SolicitacaoAluno';
+	protected $tableDB = 'SolicitacaoAluno';
 
-    abstract public function __construct();
+	abstract public function __construct();
 
-    public function dbPrepare($sql)
-    {
-        return Connection::prepare($sql);
-    }
+	public function dbPrepare($sql)
+	{
+		return Connection::prepare($sql);
+	}
 
-    public function findUnit($id)
-    {
-        $sql = "SELECT * FROM " . $this->tableDB . " WHERE id = :id";
-        $stm = $this->dbPrepare($sql);
-        $stm->bindValue(':id', $id, PDO::PARAM_INT);
-        $stm->execute();
-        return $stm->fetch();
-    }
+	public function findUnit($id)
+	{
+		$sql = "SELECT * FROM " . $this->tableDB . " WHERE id = :id";
+		$stm = $this->dbPrepare($sql);
+		$stm->bindValue(':id', $id, PDO::PARAM_INT);
+		$stm->execute();
+		return $stm->fetch();
+	}
 
-    public function findAll()
-    {
-        $sql = "SELECT * FROM  $this->tableDB";
-        $stm = $this->dbPrepare($sql);
-        $stm->execute();
-        $result = $stm->fetchAll(PDO::FETCH_OBJ);
+	public function findAll()
+	{
+		$sql = "SELECT * FROM  $this->tableDB";
+		$stm = $this->dbPrepare($sql);
+		$stm->execute();
+		$result = $stm->fetchAll(PDO::FETCH_OBJ);
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public function insert($data)
-    {
+	public function insert($data)
+	{
 //        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-        $sql = "INSERT INTO $this->tableDB (cod_aluno, servico) VALUES (:cod_aluno, :servico)";
-        $stm = $this->dbPrepare($sql);
+		$sql = "INSERT INTO $this->tableDB (cod_aluno, servico) VALUES (:cod_aluno, :servico)";
+		$stm = $this->dbPrepare($sql);
 
-        $stm->bindParam(':cod_aluno', $data['cod_cliente'], PDO::PARAM_INT);
-        $stm->bindParam(':servico', $data['servico']);
+		$stm->bindParam(':cod_aluno', $data['cod_cliente'], PDO::PARAM_INT);
+		$stm->bindParam(':servico', $data['servico']);
 
 //        foreach ($data as $key => &$value) {
 //            $stm->bindParam($key, $value);
 //        }
 
-        return $stm->execute();
-    }
+		return $stm->execute();
+	}
 
-    public function update($id)
-    {
-        $sql = "UPDATE $this->tableDB SET nome = :nome WHERE id = :id";
-        $stm = $this->dbPrepare($sql);
-        $stm->bindParam(':id', $id, PDO::PARAM_INT);
-        $stm->bindParam(':nome', $id);
-        return $stm->execute();
-    }
+	public function update($id)
+	{
+		$sql = "UPDATE $this->tableDB SET nome = :nome WHERE id = :id";
+		$stm = $this->dbPrepare($sql);
+		$stm->bindParam(':id', $id, PDO::PARAM_INT);
+		$stm->bindParam(':nome', $id);
+		return $stm->execute();
+	}
 
-    public function delete($id)
-    {
-        $sql = "DELETE FROM $this->tableDB WHERE id = :id";
-        $stm = $this->dbPrepare($sql);
-        $stm->bindParam(':id', $id, PDO::PARAM_INT);
-        return $stm->execute();
-    }
+	public function delete($id)
+	{
+		$sql = "DELETE FROM $this->tableDB WHERE id = :id";
+		$stm = $this->dbPrepare($sql);
+		$stm->bindParam(':id', $id, PDO::PARAM_INT);
+		return $stm->execute();
+	}
 
 }
--- database/Connection.php
+++ database/Connection.php
@@ -11,7 +11,7 @@ class Connection
         try {
             if (is_null(self::$pdo)) {
                 self::$pdo = new PDO(
-                    'mysql:host='.DB_HOST.';dbname='.DB_NAME,
+                    'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
                     DB_USER,
                     DB_PASS);
                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
